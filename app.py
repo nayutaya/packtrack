1e0c05ade70cb9acbe2081aa14f22ff405f85767
Yuya Kato <yuyakato@gmail.com>
2009-08-24 12:10:28 +0900

    HTMLのパース処理を整理

--- app.py
+++ app.py
@@ -8,38 +8,24 @@ from google.appengine.api import urlfetch
 import tracker.sagawa
 
 
-#print "Content-Type: text/plain"
-print "Content-Type: text/html"
+print "Content-Type: text/plain"
+#print "Content-Type: text/html"
 print ""
 
 #print "hello"
 
-url    = "http://k2k.sagawa-exp.co.jp/p/sagawa/web/okurijoinput.jsp"
-result = urlfetch.fetch(url = url)
+result = tracker.sagawa.get_first_page()
 
-#print result.status_code
+print result.status_code
 #print result.content
 
-#regexp = re.compile(r"<input type=\"hidden\" name=\"jsf_tree_64\".+value=\".+?\"", re.IGNORECASE | re.DOTALL)
-#match  = regexp.search(result.content)
-#print match
-#print match.group(0)
+#print tracker.sagawa.parse_forms(result.content)
+fields = tracker.sagawa.get_input_fields(result.content)
+for field in fields:
+  print field
 
-def get_input_fragments(src):
-  regexp = re.compile(r"<input.+?>", re.IGNORECASE | re.DOTALL)
-  return regexp.findall(src)
+exit(0)
 
-def get_input_fields(fragments):
-  pattern = re.compile(r" (.*?)=\"(.*?)\"")
-
-  results = []
-  for fragment in fragments:
-    hash = {}
-    for key, value in pattern.findall(fragment):
-      hash[key] = value
-    results.append(hash)
-
-  return results
 
 input_fragments = get_input_fragments(result.content)
 input_fields    = get_input_fields(input_fragments)
--- tracker/sagawa.py
+++ tracker/sagawa.py
@@ -1,4 +1,35 @@
 # -*- coding: utf-8 -*-
 
-def hello():
-  return "hello!!"
+import re
+from google.appengine.api import urlfetch
+
+
+def create_first_page_url():
+  return "http://k2k.sagawa-exp.co.jp/p/sagawa/web/okurijoinput.jsp"
+
+def get_first_page():
+  url = create_first_page_url()
+  return urlfetch.fetch(url = url)
+
+#def parse_forms(html):
+#  pattern = re.compile(r"<form(.*?)>(.*?)</form>", re.IGNORECASE | re.DOTALL)
+#  return pattern.findall(html)
+
+def get_input_fields(html):
+  pattern = re.compile(r"<input(.+?)>", re.IGNORECASE | re.DOTALL)
+
+  results = []
+  for fragment in pattern.findall(html):
+    attrs = parse_attributes(fragment)
+    results.append(attrs)
+
+  return results
+
+def parse_attributes(html):
+  pattern = re.compile(r" (.+?)=\"(.+?)\"")
+
+  result = {}
+  for key, value in pattern.findall(html):
+    result[key] = value
+
+  return result
