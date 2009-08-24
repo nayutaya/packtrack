57cbecfc1c191fdf7e0deec633b625a609b2f342
Yuya Kato <yuyakato@gmail.com>
2009-08-24 11:48:49 +0900

    配送状況の取得処理を試験的に実装

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import logging
 import urllib
 from google.appengine.api import urlfetch
 
-print "Content-Type: text/plain"
+#print "Content-Type: text/plain"
+print "Content-Type: text/html"
 print ""
 
 #print "hello"
@@ -13,7 +14,7 @@ print ""
 url    = "http://k2k.sagawa-exp.co.jp/p/sagawa/web/okurijoinput.jsp"
 result = urlfetch.fetch(url = url)
 
-print result.status_code
+#print result.status_code
 #print result.content
 
 #regexp = re.compile(r"<input type=\"hidden\" name=\"jsf_tree_64\".+value=\".+?\"", re.IGNORECASE | re.DOTALL)
@@ -30,29 +31,39 @@ def get_input_fields(fragments):
 
   results = []
   for fragment in fragments:
-    results.append(pattern.findall(fragment))
+    hash = {}
+    for key, value in pattern.findall(fragment):
+      hash[key] = value
+    results.append(hash)
 
   return results
 
 input_fragments = get_input_fragments(result.content)
 input_fields    = get_input_fields(input_fragments)
-for input_fragment in input_fragments:
-  print input_fragment
+#for input_fragment in input_fragments:
+#  print input_fragment
+"""
 print "----------"
 for input_field in input_fields:
   print input_field
-
-
 """
+
+#print "----------"
+hash = {}
+for input_field in input_fields:
+  name  = input_field.get("name")
+  value = input_field.get("value", "")
+  hash[name] = value
+#print hash
+
+
+hash["main:no1"] = "600097281033"
+hash["main:no2"] = "600092368315"
+
 url = "http://k2k.sagawa-exp.co.jp/p/sagawa/web/okurijoinput.jsp"
-hash = {
-  "main:no1": "600097281033",
-  "main:no2": "600092368315",
-}
 data = urllib.urlencode(hash)
 result = urlfetch.fetch(url = url, method = urlfetch.POST, payload = data)
 
+#print "----------"
 #print result.status_code
-
 print result.content
-"""
