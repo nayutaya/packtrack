f9c3d9471e393a49b9787590a60e7961e4688aea
Yuya Kato <yuyakato@gmail.com>
2009-08-25 23:59:36 +0900

    伝票番号を引数で指定できるように変更

--- tracker/jppost.py
+++ tracker/jppost.py
@@ -6,8 +6,6 @@ import urllib2
 
 def create_list_page_base_params():
   return {
-    "JSESSIONID": "KTzQblRXyvQWJh1BzkTm3YZzyszpMwyYf81fSpZypTVGjQVLLdkK!1995213173!1251210176530",
-    "org.apache.struts.taglib.html.TOKEN": "825c0b1b950dbb316bcd2c66576e42e7",
     "SVID": "020",
     "locale": "ja",
     "searchKind": "S002",
@@ -23,35 +21,35 @@ def create_list_page_base_params():
     "reqCodeNo10": "",
   }
 
-def create_list_page_number_params():
-  return {
-    "reqCodeNo1": "317443794205",
-    "reqCodeNo2": "317443794334",
-  }
+def create_list_page_number_params(numbers):
+  params = {}
+  for i, number in enumerate(numbers[:10]):
+    params["reqCodeNo%i" % (i + 1)] = number
+  return params
 
-def create_list_page_params():
+def create_list_page_params(numbers):
   params = create_list_page_base_params()
-  params.update(create_list_page_number_params())
+  params.update(create_list_page_number_params(numbers))
   return params
 
 def create_list_page_base_url():
   return "http://tracking.post.japanpost.jp/service/singleSearch.do"
 
-def create_list_page_url():
+def create_list_page_url(numbers):
   base   = create_list_page_base_url()
-  params = create_list_page_params()
+  params = create_list_page_params(numbers)
   return base + "?" + urllib.urlencode(params)
 
-def create_list_page_request():
+def create_list_page_request(numbers):
   return urllib2.Request(
-    url = create_list_page_url())
+    url = create_list_page_url(numbers))
 
-def open_list_page():
-  request = create_list_page_request()
+def open_list_page(numbers):
+  request = create_list_page_request(numbers)
   return urllib2.urlopen(request)
 
-def get_list_page():
-  io = open_list_page()
+def get_list_page(numbers):
+  io = open_list_page(numbers)
   page = io.read()
   io.close()
   return page
--- youpack.py
+++ youpack.py
@@ -6,7 +6,12 @@ from tracker import jppost
 #print jppost.create_list_page_request()
 #print jppost.open_list_page()
 
-page = jppost.get_list_page()
+numbers = [
+  "317443794205",
+  "317443794334",
+]
+
+page = jppost.get_list_page(numbers)
 f = open("page.html", "wb")
 f.write(page)
 f.close()
