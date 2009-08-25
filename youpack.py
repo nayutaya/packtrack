39fa42dc82581f9781a2ebf3d03e39efa07d9ec9
Yuya Kato <yuyakato@gmail.com>
2009-08-25 23:34:06 +0900

    ゆうパックの荷物追跡スクリプトを追加

---
+++ youpack.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from tracker import jppost
+
+print jppost.create_list_page_base_url()
+print jppost.create_list_page_base_params()
+print jppost.create_list_page_number_params()
