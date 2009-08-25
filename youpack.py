98bd5de4b30bb3c6d28fa7d343b6b6fb1149bb38
Yuya Kato <yuyakato@gmail.com>
2009-08-26 02:17:46 +0900

    ElementTreeクラスを使って解析するように変更

--- youpack.py
+++ youpack.py
@@ -30,17 +30,43 @@ from BeautifulSoup import BeautifulSoup
 soup = BeautifulSoup(page)
 #print soup.prettify()
 
-body = soup.body
-
-table = body("table", {"align": "center"})[0]
-tr    = table("tr")[0]
-td    = tr("td")[0]
-print td.renderContents().strip()
+#body = soup.body
+#def get_response_time(body):
+#  table = body("table")[0]
+#  tr    = table("tr")[0]
+#  td    = tr("td")[0]
+#  return td.renderContents().strip()
+#print ("time", get_response_time(body))
 
 #for x in body("table", {"align": "center"}):
 #  print x
 
-out = body.prettify()
+#out = body.prettify()
+
+# 値のない属性はエラーとなるため、値を与える
+tds = soup.findAll("td", {"nowrap": None})
+for td in tds:
+  for i, (key, value) in enumerate(td.attrs):
+    if key == "nowrap":
+      td.attrs[i] = (key, key)
+
+import xml.etree.ElementTree as etree
+html = etree.fromstring(soup.prettify())
+
+#out = soup.prettify()
+
+body = html.find("body")
+
+def get_response_time(body):
+  table = body.findall("table")[0]
+  tr    = table.find("tr")
+  td    = tr.find("td")
+  return td.text.strip()
+
+print get_response_time(body)
+
+out = etree.tostring(body)
+
 f = open("out.html", "wb")
 f.write(out)
 f.close()
