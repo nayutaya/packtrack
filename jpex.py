55324c6f29771a0937de33509f448de5ea0d59ed
Yuya Kato <yuyakato@gmail.com>
2009-08-25 11:43:16 +0900

    パラメータの生成処理を追加

--- jpex.py
+++ jpex.py
@@ -2,8 +2,6 @@
 
 from tracker import jpexpress
 
-#print jpexpress.get_first_page_url()
-#print jpexpress.create_first_page_request()
 #io = jpexpress.open_first_page()
 #page1 = io.read()
 
@@ -16,4 +14,23 @@ page1 = f.read()
 f.close()
 
 #print page1
-print jpexpress.get_session_id(page1)
+session_id = jpexpress.get_session_id(page1)
+print session_id
+
+url = jpexpress.create_list_page_url(session_id)
+print url
+
+#params = jpexpress.create_list_page_base_params()
+#print params
+
+#a = ["a", "b"]
+#for x, y in enumerate(a):
+#  print (x, y)
+#a = [1,2,3]
+#print a[:4]
+
+#params2 = jpexpress.create_list_page_number_params(["a","b","c","d","e","a","b","c","d","e","f"])
+#print params2
+
+params = jpexpress.create_list_page_params(["a","b","c","d","e","a","b","c","d","e","f"])
+print params
--- tracker/jpexpress.py
+++ tracker/jpexpress.py
@@ -5,12 +5,12 @@ import urllib
 import urllib2
 
 
-def get_first_page_url():
+def create_first_page_url():
   return "http://info.jpexpress.jp/confirm/confirmIndex.html"
 
 def create_first_page_request():
   return urllib2.Request(
-    url = get_first_page_url())
+    url = create_first_page_url())
 
 def open_first_page():
   request = create_first_page_request()
@@ -23,3 +23,33 @@ def get_session_id(html):
     return match.group(1)
   else:
     return None
+
+def create_list_page_url(session_id):
+  return "http://info.jpexpress.jp/confirm/confirmIndex.html;jsessionid=" + session_id
+
+def create_list_page_base_params():
+  return {
+    "includeChildBody:confirmIndexForm:doConfirmCareerList": "",
+    "includeChildBody:confirmIndexForm/confirm/confirmIndex.html": "includeChildBody:confirmIndexForm",
+    "includeChildBody:confirmIndexForm:denpyoNo0": "",
+    "includeChildBody:confirmIndexForm:denpyoNo1": "",
+    "includeChildBody:confirmIndexForm:denpyoNo2": "",
+    "includeChildBody:confirmIndexForm:denpyoNo3": "",
+    "includeChildBody:confirmIndexForm:denpyoNo4": "",
+    "includeChildBody:confirmIndexForm:denpyoNo5": "",
+    "includeChildBody:confirmIndexForm:denpyoNo6": "",
+    "includeChildBody:confirmIndexForm:denpyoNo7": "",
+    "includeChildBody:confirmIndexForm:denpyoNo8": "",
+    "includeChildBody:confirmIndexForm:denpyoNo9": "",
+  }
+
+def create_list_page_number_params(numbers):
+  params = {}
+  for i, number in enumerate(numbers[:10]):
+    params["includeChildBody:confirmIndexForm:denpyoNo%i" % i] = number
+  return params
+
+def create_list_page_params(numbers):
+  params = create_list_page_base_params()
+  params.update(create_list_page_number_params(numbers))
+  return params
