331ffd127da68eac07adc77979c70b889102b088
Yuya Kato <yuyakato@gmail.com>
2009-08-26 11:48:05 +0900

    情報取得処理を整理

--- hikyaku.py
+++ hikyaku.py
@@ -7,7 +7,19 @@ numbers = [
   "600092368315"
 ]
 
-#print sagawa.get_first_page()
+fields = sagawa.get_first_page_params()
+state = fields["jsf_state_64"]
+tree  = fields["jsf_tree_64"]
+
+params = sagawa.PackageDetailPage.create_params(state, tree, numbers)
+#print params
+
+result = sagawa.get_detail_page(params)
+print result
+
+exit(0)
+
+
 
 params = sagawa.get_first_page_params()
 params.update(sagawa.create_detail_page_number_params(numbers))
--- tracker/sagawa.py
+++ tracker/sagawa.py
@@ -31,6 +31,55 @@ class PackageFirstPage:
     return page
 
 
+class PackageDetailPage:
+  def __init__(self):
+    pass
+
+  @classmethod
+  def create_detail_page_url(cls):
+    return "http://k2k.sagawa-exp.co.jp/p/sagawa/web/okurijoinput.jsp"
+
+  @classmethod
+  def create_detail_page_base_params(cls):
+    return {
+      "jsf_viewid": "/web/okurijoinput.jsp",
+      "main:_link_hidden_": "",
+      "main:correlation": "1",
+      "main:no1": "",
+      "main:no2": "",
+      "main:no3": "",
+      "main:no4": "",
+      "main:no5": "",
+      "main:no6": "",
+      "main:no7": "",
+      "main:no8": "",
+      "main:no9": "",
+      "main:no10": "",
+      "main_SUBMIT": "1",
+      "main:toiStart": "",
+    }
+
+  @classmethod
+  def create_detail_page_system_params(cls, state, tree):
+    return {
+      "jsf_state_64": state,
+      "jsf_tree_64": tree,
+    }
+
+  @classmethod
+  def create_detail_page_number_params(cls, numbers):
+    params = {}
+    for i, number in enumerate(numbers[:10]):
+      params["main:no%i" % (i + 1)] = number
+    return params
+
+  @classmethod
+  def create_params(cls, state, tree, numbers):
+    params = cls.create_detail_page_base_params()
+    params.update(cls.create_detail_page_system_params(state, tree))
+    params.update(cls.create_detail_page_number_params(numbers))
+    return params
+
 
 def get_input_fields(html):
   pattern = re.compile(r"<input(.+?)>", re.IGNORECASE | re.DOTALL)
@@ -64,20 +113,12 @@ def get_first_page_params():
   html = cls.get_content()
   return parse_first_page_params(html)
 
-def create_detail_page_url():
-  return "http://k2k.sagawa-exp.co.jp/p/sagawa/web/okurijoinput.jsp"
 
-def create_detail_page_number_params(numbers):
-  num = 1
-  result = {}
-  for number in numbers:
-    result["main:no%i" % num] = number
-    num += 1
-  return result
 
 def create_detail_page_request(params):
+  cls = PackageDetailPage
   return urllib2.Request(
-    url  = create_detail_page_url(),
+    url  = cls.create_detail_page_url(),
     data = urllib.urlencode(params))
 
 def open_detail_page(params):
