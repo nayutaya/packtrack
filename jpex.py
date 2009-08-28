4f22adbda06083b2e0a05a333a9a30d6b03b95b6
Yuya Kato <yuyakato@gmail.com>
2009-08-28 14:20:56 +0900

    50件のランダムな追跡番号に対して問い合わせを行う処理を追加

--- jpex.py
+++ jpex.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import re
+import sys
 from tracker import jpexpress
 
 session = jpexpress.PackageTrackingSession()
@@ -8,28 +8,31 @@ session = jpexpress.PackageTrackingSession()
 
 #numbers = ["348012244355", "348011824893", "348011053121"]
 
-from tracker import jppost
-numbers = []
-for i in range(10):
-  number = jppost.PackageTrackingNumber.create_random_number("3480")
-  numbers.append(number)
+for i in range(5):
+  sys.stderr.write(".")
 
-page = session.get_list_page(numbers)
-#print page
-f = open("page2.html", "wb")
-f.write(page.content)
-f.close()
+  from tracker import jppost
+  numbers = []
+  for i in range(10):
+    number = jppost.PackageTrackingNumber.create_random_number("3480")
+    numbers.append(number)
 
-page_info = jpexpress.PackageListPageParser.parse(page.content)
-#print page_info
+  page = session.get_list_page(numbers)
+  #print page
+  f = open("page2.html", "wb")
+  f.write(page.content)
+  f.close()
 
-for record in page_info["list"]:
-  line  = ""
-  line += (record[u"No"]                 ) + "\t"
-  line += (record[u"送り状番号"]         ) + "\t"
-  line += (record[u"最新状況"]      or "") + "\t"
-  line += (record[u"最新状況:日時"] or "") + "\t"
-  line += (record[u"受付日"]        or "") + "\t"
-  line += (record[u"お届け指定日"]  or "") + "\t"
-  line += (record[u"扱区分"]        or "")
-  print line
+  page_info = jpexpress.PackageListPageParser.parse(page.content)
+  #print page_info
+
+  for record in page_info["list"]:
+    line  = ""
+    line += (record[u"No"]                 ) + "\t"
+    line += (record[u"送り状番号"]         ) + "\t"
+    line += (record[u"最新状況"]      or "") + "\t"
+    line += (record[u"最新状況:日時"] or "") + "\t"
+    line += (record[u"受付日"]        or "") + "\t"
+    line += (record[u"お届け指定日"]  or "") + "\t"
+    line += (record[u"扱区分"]        or "")
+    print line.encode("shift-jis")
