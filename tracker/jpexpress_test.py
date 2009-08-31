a89500ea98ddba7106527122ac1675d7bc6c8d54
Yuya Kato <yuyakato@gmail.com>
2009-08-31 11:37:45 +0900

    各種状態のテストを追加

--- tracker/jpexpress_test.py
+++ tracker/jpexpress_test.py
@@ -146,6 +146,78 @@ class TestPackageListPageParser(unittest.TestCase):
     actual = jpexpress.PackageListPageParser.parse(self.read_data("list_count02.html"))
     self.assertEqual(expected, actual)
 
+  def test_parse__accepted(self):
+    expected = {
+      "list": [
+        {
+          u"No"                 : u"1",
+          u"送り状番号"         : u"348-01-225-3676",
+          u"送り状番号:リンク先": u"confirmDetail.html;jsessionid=8HDS3U7DKEVI3KOKP4BLEO1A69GU03V05N0I3F5FMQMFFB9I23PITSBKBSHG2000N4000000.WU001_001?denpyoNo=348-01-225-3676&detailQuery=pspaksba000_000.jsp%3Fuji.verb%3Dstartup%26uji.bean%3Dnittsu.kzd.pspakzdj002_501%26stbInvoiceNo%3D348012253676%26stbDupulicationCheckNo%3D0%26stbInvoiceDSI%3D033%26stbMonthDSI%3D08%26st",
+          u"最新状況"           : u"受取が完了いたしました。",
+          u"最新状況:日時"      : u"8/10 19:41",
+          u"受付日"             : u"8/ 6",
+          u"お届け指定日"       : u"時間指定なし",
+          u"扱区分"             : u"ペリカン便",
+        },
+      ],
+    }
+    actual = jpexpress.PackageListPageParser.parse(self.read_data("list_accepted.html"))
+    self.assertEqual(expected, actual)
+
+  def test_parse__arrival(self):
+    expected = {
+      "list": [
+        {
+          u"No"                 : u"1",
+          u"送り状番号"         : u"348-00-190-9286",
+          u"送り状番号:リンク先": u"confirmDetail.html;jsessionid=7L1V11KGCDJ1EP66J7FGLJDPSDGU03V05N0I3F5FE7OC3TA6590BB13MBSHG20000K000000.WU001_003?denpyoNo=348-00-190-9286&detailQuery=pspaksba000_000.jsp%3Fuji.verb%3Dstartup%26uji.bean%3Dnittsu.kzd.pspakzdj002_501%26stbInvoiceNo%3D348001909286%26stbDupulicationCheckNo%3D0%26stbInvoiceDSI%3D014%26stbMonthDSI%3D08%26st",
+          u"最新状況"           : u"コンビニ配送センターに到着いたしました。08月27日の１８時以降にご指定の店舗でお受け取りできます。",
+          u"最新状況:日時"      : u"8/27",
+          u"受付日"             : u"8/26",
+          u"お届け指定日"       : u"時間指定なし",
+          u"扱区分"             : u"ペリカン便",
+        },
+      ],
+    }
+    actual = jpexpress.PackageListPageParser.parse(self.read_data("list_arrival.html"))
+    self.assertEqual(expected, actual)
+
+  def test_parse__delivery(self):
+    expected = {
+      "list": [
+        {
+          u"No"                 : u"1",
+          u"送り状番号"         : u"380-73-835-2890",
+          u"送り状番号:リンク先": u"confirmDetail.html;jsessionid=GH12IVIGT6J0QL1HJAANM7TFHDGU03V05N0I3F1LQ5DPNRJ84ISJDIJRBSHG20003O000000.WU001_004?denpyoNo=380-73-835-2890&detailQuery=pspaksba000_000.jsp%3Fuji.verb%3Dstartup%26uji.bean%3Dnittsu.kzd.pspakzdj002_501%26stbInvoiceNo%3D380738352890%26stbDupulicationCheckNo%3D0%26stbInvoiceDSI%3D044%26stbMonthDSI%3D07%26st",
+          u"最新状況"           : u"配達完了いたしました。",
+          u"最新状況:日時"      : u"7/6 8:57",
+          u"受付日"             : u"7/ 4",
+          u"お届け指定日"       : None,
+          u"扱区分"             : u"ペリカン便",
+        },
+      ],
+    }
+    actual = jpexpress.PackageListPageParser.parse(self.read_data("list_delivery.html"))
+    self.assertEqual(expected, actual)
+
+  def test_parse__inquiry(self):
+    expected = {
+      "list": [
+        {
+          u"No"                 : u"1",
+          u"送り状番号"         : u"348-00-178-6042",
+          u"送り状番号:リンク先": u"confirmDetail.html;jsessionid=EE1J77NDEDH904G2B5QIN4QJJLGU03V05N0I3F1EUKH4D3RP0DAOU73NBSHG2000TC000000.WU001_003?denpyoNo=348-00-178-6042&detailQuery=pspaksba000_000.jsp%3Fuji.verb%3Dstartup%26uji.bean%3Dnittsu.kzd.pspakzdj002_501%26stbInvoiceNo%3D348001786042%26stbDupulicationCheckNo%3D0%26stbInvoiceDSI%3D002%26stbMonthDSI%3D07%26st",
+          u"最新状況"           : u"弊社お問い合わせ窓口店にお問合わせ下さい。",
+          u"最新状況:日時"      : u"7/21",
+          u"受付日"             : u"7/10",
+          u"お届け指定日"       : u"時間指定なし",
+          u"扱区分"             : u"ペリカン便",
+        },
+      ],
+    }
+    actual = jpexpress.PackageListPageParser.parse(self.read_data("list_inquiry.html"))
+    self.assertEqual(expected, actual)
+
 
 # TODO: PackageDetailPageクラスのテストを記述
 
