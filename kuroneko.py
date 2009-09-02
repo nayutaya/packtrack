8312627490d79534970abe63f1f64bdf9cdad64e
Yuya Kato <yuyakato@gmail.com>
2009-09-02 17:33:57 +0900

    不要な要素を削除する処理を詳細ページ解析クラスに移動

--- kuroneko.py
+++ kuroneko.py
@@ -26,58 +26,24 @@ f = open("src1.html", "wb")
 f.write(src1)
 f.close()
 
-src2 = DetailPageParser.trim_script_tag(src1)
+src2 = DetailPageParser.trim_unwanted_tags(src1)
 f = open("src2.html", "wb")
 f.write(src2)
 f.close()
 
-src3 = DetailPageParser.trim_style_tag(src2)
-f = open("src3.html", "wb")
-f.write(src3)
-f.close()
 
-src4 = DetailPageParser.trim_bold_tag(DetailPageParser.trim_paragraph_tag(DetailPageParser.trim_center_tag(src3)))
-f = open("src4.html", "wb")
-f.write(src4)
-f.close()
-
-doc = BeautifulSoup(src4)
+doc = BeautifulSoup(src2)
 f = open("tmp.html", "wb")
 f.write(doc.prettify())
 f.close()
-
-# ヘッダを削除
-for i in range(4):
-  doc.body.table.extract()
-doc.body.form.extract()
-for i in range(4):
-  doc.body.table.extract()
-
-# フッタを削除
-doc.body.findAll("table", recursive = False)[-1].extract()
-
-# リンクを削除
-for elem in doc.body.findAll("a", {"name": re.compile(".+")}):
-  elem.extract()
-for elem in doc.body.findAll("a", {"href": re.compile("^#")}):
-  elem.extract()
-
-# ボタンを削除
-for elem in doc.body.findAll("div", {"class": "print_hide"}):
-  elem.extract()
-
-# 水平線を削除
-for elem in doc.body.findAll("hr"):
-  elem.extract()
-
-# 強制改行を削除
-for elem in doc.body.findAll("br"):
-  elem.extract()
 
 f = open("tmp2.html", "wb")
 f.write(doc.prettify())
 f.close()
 
+doc = BeautifulSoup(src2)
+
+DetailPageParser.trim_unwanted_nodes(doc)
 
 f = open("tmp3.html", "wb")
 f.write(doc.prettify())
--- tracker/yamato/package/detail_page_parser.py
+++ tracker/yamato/package/detail_page_parser.py
@@ -32,3 +32,42 @@ class DetailPageParser:
   def trim_bold_tag(cls, html):
     pattern = re.compile(r"</?b>", re.IGNORECASE)
     return re.sub(pattern, "", html)
+
+  @classmethod
+  def trim_unwanted_tags(cls, html):
+    result1 = cls.trim_script_tag(html)
+    result2 = cls.trim_style_tag(result1)
+    result3 = cls.trim_center_tag(result2)
+    result4 = cls.trim_paragraph_tag(result3)
+    result5 = cls.trim_bold_tag(result4)
+    return result5
+
+  @classmethod
+  def trim_unwanted_nodes(cls, doc):
+    # ヘッダを削除
+    for i in range(4):
+      doc.body.table.extract()
+    doc.body.form.extract()
+    for i in range(4):
+      doc.body.table.extract()
+
+    # フッタを削除
+    doc.body.findAll("table", recursive = False)[-1].extract()
+
+    # リンクを削除
+    for elem in doc.body.findAll("a", {"name": re.compile(".+")}):
+      elem.extract()
+    for elem in doc.body.findAll("a", {"href": re.compile("^#")}):
+      elem.extract()
+
+    # ボタンを削除
+    for elem in doc.body.findAll("div", {"class": "print_hide"}):
+      elem.extract()
+
+    # 水平線を削除
+    for elem in doc.body.findAll("hr"):
+      elem.extract()
+
+    # 強制改行を削除
+    for elem in doc.body.findAll("br"):
+      elem.extract()
