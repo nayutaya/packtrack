c0862f4406e552aa152bcbe110e5ebbdaea0cc57
Yuya Kato <yuyakato@gmail.com>
2009-09-02 23:34:21 +0900

    実装を整理

--- kuroneko.py
+++ kuroneko.py
@@ -1,47 +1,30 @@
 # -*- coding: utf-8 -*-
 
 from tracker.yamato.package.session import Session
-#from tracker.yamato.package.detail_page_fetcher import DetailPageFetcher
-#from tracker.yamato.package.detail_page_parser import DetailPageParser
+from tracker.yamato.package.list_page_fetcher import ListPageFetcher
+from tracker.yamato.package.list_page_parser import ListPageParser
 
 
 session = Session()
-print session.get_list(["225303520584", "249790484403"])
+#print session.get_list(["225303520584", "249790484403"])
 
-exit(0)
+numbers = [
+  "249724973934",
+]
 
-"""
-if False:
-  numbers = ["225303520584", "249790484403"]
-  detail_page = DetailPageFetcher.get_content(numbers)
-  f = open("yamato.html", "wb")
-  f.write(detail_page)
-  f.close()
-else:
-  f = open("yamato.html", "rb")
-  detail_page = f.read()
-  f.close()
-"""
-
-import re
-from BeautifulSoup import BeautifulSoup
-
-f = open("tracker/yamato/package/fixtures/detail_count02.html", "rb")
-html = f.read()
-f.close()
-#print html
-
-doc = DetailPageParser.create_doc(html)
-f = open("tmp.html", "wb")
-f.write(doc.prettify())
+page = ListPageFetcher.get_content(numbers)
+f = open("yamato.html", "wb")
+f.write(page)
 f.close()
 
-info = DetailPageParser.parse(html)
+info = ListPageParser.parse(page)
+#print info
+
 for item1 in info[u"一覧"]:
   for key1, value1 in item1.items():
     if isinstance(value1, basestring):
       print key1 + ": '" + value1 + "'"
-    else:
+    elif value1 is not None:
       print key1 + ":"
       for item2 in value1:
         for key2, value2 in item2.items():
