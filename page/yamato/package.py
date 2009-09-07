f01dac8092932dcddfea83d692c6af42771a721c
Yuya Kato <yuyakato@gmail.com>
2009-09-07 12:12:28 +0900

    問い合わせ処理を追加

--- page/yamato/package.py
+++ page/yamato/package.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
@@ -14,13 +15,29 @@ class QueryPage(webapp.RequestHandler):
     number_params = [{"name": name, "value": self.request.get(name)} for name in param_names]
     numbers       = [param["value"] for param in number_params]
 
+    valid_numbers = [number for number in numbers if TrackingNumber.is_valid(number)]
+    session = Session()
+    list    = session.get_list(valid_numbers)
+
+    table = {}
+    for record in list[u"一覧"]:
+      tracking_number = re.sub("-", "", record[u"伝票番号"])
+      message         = unicode(record[u"メッセージ"])
+      table[tracking_number] = {
+        "message": message,
+      }
+
     results = []
     for number in numbers:
       if number == "":
         continue
 
       if TrackingNumber.is_valid(number):
-        message = "正常"
+        record = table.get(number)
+        if record is not None:
+          message = record["message"]
+        else:
+          message = "情報なし"
       else:
         message = "不正な番号です"
       hash = {
