83d88acc6ee9dde8de205ffad2306e12d2ff4260
Yuya Kato <yuyakato@gmail.com>
2009-09-09 15:49:09 +0900

    すべてのファイルをコンパイルするため、import文を追加

--- tracker/sagawa/package/first_page_parser_test.py
+++ tracker/sagawa/package/first_page_parser_test.py
@@ -2,6 +2,8 @@
 
 import unittest
 
+import first_page_parser
+
 #class TestXXX(unittest.TestCase):
 #  def setUp(self):
 #    pass
--- tracker/sagawa/package/tracking_number_test.py
+++ tracker/sagawa/package/tracking_number_test.py
@@ -2,6 +2,8 @@
 
 import unittest
 
+import tracking_number
+
 #class TestXXX(unittest.TestCase):
 #  def setUp(self):
 #    pass
