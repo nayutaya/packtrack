2126b6eb37450ae0ea5a81b45c2edb9b945f336d
Yuya Kato <yuyakato@gmail.com>
2009-09-09 15:14:11 +0900

    テストをまとめて実行するためのスクリプトを追加

---
+++ tracker/jpexpress/package/alltests.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import detail_page_fetcher_test
+import detail_page_parser_test
+import first_page_fetcher_test
+import first_page_parser_test
+import list_page_fetcher_test
+import list_page_parser_test
+import session_test
+import tracking_number_test
+
+def suite():
+  loader = unittest.defaultTestLoader.loadTestsFromModule
+  suites = [
+    loader(detail_page_fetcher_test),
+    loader(detail_page_parser_test),
+    loader(first_page_fetcher_test),
+    loader(first_page_parser_test),
+    loader(list_page_fetcher_test),
+    loader(list_page_parser_test),
+    loader(session_test),
+    loader(tracking_number_test),
+  ]
+  return unittest.TestSuite(suites)
+
+if __name__ == "__main__":
+  unittest.TextTestRunner(verbosity = 1).run(suite())
