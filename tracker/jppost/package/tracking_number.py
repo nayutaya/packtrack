d2d2dadef7c968e69f4a7cb8b34e00d26a9c3732
Yuya Kato <yuyakato@gmail.com>
2009-09-10 11:26:33 +0900

    追跡番号クラスをyamatoモジュールからコピー

--- tracker/jppost/package/tracking_number.py
+++ tracker/jppost/package/tracking_number.py
@@ -1,1 +1,33 @@
 # -*- coding: utf-8 -*-
+
+import re
+import random
+
+# 追跡番号クラス
+class TrackingNumber:
+  pattern = re.compile(r"^[0-9]{12}$")
+
+  def __init__(self):
+    pass
+
+  @classmethod
+  def create_check_digit(cls, body_digits):
+    return str(int(body_digits) % 7)
+
+  @classmethod
+  def split_check_digit(cls, digits):
+    return (digits[0:11], digits[11:12])
+
+  @classmethod
+  def is_valid(cls, digits):
+    if cls.pattern.match(digits) is None: return False
+    body_digits, check_digit = cls.split_check_digit(digits)
+    if check_digit != cls.create_check_digit(body_digits): return False
+    return True
+
+  @classmethod
+  def create_random_number(cls, prefix = ""):
+    number = prefix
+    while len(number) < 11:
+      number += str(random.randint(0, 9))
+    return number + cls.create_check_digit(number)
--- tracker/jppost/package/tracking_number_test.py
+++ tracker/jppost/package/tracking_number_test.py
@@ -2,9 +2,41 @@
 
 import unittest
 
-#class TestXXX(unittest.TestCase):
-#  def setUp(self):
-#    pass
+from tracking_number import TrackingNumber
+
+class TestTrackingNumber(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  def test_create_check_digit(self):
+    target = TrackingNumber.create_check_digit
+    self.assertEqual("0", target("00000000000"))
+    self.assertEqual("6", target("00000000006"))
+    self.assertEqual("0", target("00000000007"))
+    self.assertEqual("4", target("99999999999"))
+
+  def test_split_check_digit(self):
+    target = TrackingNumber.split_check_digit
+    self.assertEqual(("00000000000", "0"), target("000000000000"))
+    self.assertEqual(("01234567890", "1"), target("012345678901"))
+
+  def test_is_valid(self):
+    target = TrackingNumber.is_valid
+    self.assertEqual(True, target("000000000000"))
+    self.assertEqual(True, target("012345678903"))
+    self.assertEqual(True, target("999999999994"))
+
+    self.assertEqual(False, target("00000000000"))
+    self.assertEqual(False, target("0000000000000"))
+    self.assertEqual(False, target("aaaaaaaaaaaa"))
+    self.assertEqual(False, target("000000000001"))
+
+  def test_create_random_number(self):
+    target = TrackingNumber.create_random_number
+    self.assertEqual(False, (target() == target()))
+    self.assertEqual(True, TrackingNumber.is_valid(target()))
+    self.assertEqual("0",  target("0")[:1])
+    self.assertEqual("12", target("12")[:2])
 
 if __name__ == "__main__":
   unittest.main()
