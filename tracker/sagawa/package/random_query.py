af25749508cefb966f93d5c46ca49882c002123d
Yuya Kato <yuyakato@gmail.com>
2009-09-14 12:09:31 +0900

    セッションクラスを使うように変更

--- tracker/sagawa/package/random_query.py
+++ tracker/sagawa/package/random_query.py
@@ -2,22 +2,17 @@
 
 import sys
 from tracking_number import TrackingNumber
-from first_page_fetcher import FirstPageFetcher
-from first_page_parser import FirstPageParser
-from list_page_fetcher import ListPageFetcher
+from session import Session
 
 number_prefix = "6000"
+
+session = Session()
 
 numbers = [TrackingNumber.create_random_number(number_prefix) for i in range(10)]
 for number in numbers:
   sys.stderr.write(number + "\n")
 
-page = FirstPageFetcher.get()
-data = FirstPageParser.parse(page.content)
-state = data["jsf_state_64"]
-tree  = data["jsf_tree_64"]
-
-list = ListPageFetcher.get(state, tree, numbers)
+list = session.get_list_page(numbers)
 
 f = open("tmp.html", "wb")
 f.write(list.content)
