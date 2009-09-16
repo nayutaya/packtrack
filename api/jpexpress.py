244b017381c47a9ac5590d383c62f78e2733ba16
Yuya Kato <yuyakato@gmail.com>
2009-09-16 12:14:46 +0900

    パラメータの処理を追加

--- api/jpexpress.py
+++ api/jpexpress.py
@@ -5,24 +5,30 @@ from google.appengine.ext import webapp
 
 class PackageListJson(webapp.RequestHandler):
   def get(self):
+    param_numbers = self.request.get("numbers")
+    array_numbers = param_numbers.split(",")
+
+    result = {}
+    for number in array_numbers:
+      ret = {
+        "message": u"配達完了いたしました。",
+        "current_state": u"配達完了いたしました。",
+        "current_state_time": u"8/12 9:19",
+        "type": u"ペリカン便",
+        "detail": {
+          "reception_date": "8/10",
+          "delivery_time": u"8/12 午前中",
+        },
+      }
+      result[number] = ret
+
     output = {
       "success": True,
       "parameter": {
         "callback": None,
-        "numbers": ["144856020890"],
+        "numbers": array_numbers,
       },
-      "result": {
-        "144856020890": {
-          "message": u"配達完了いたしました。",
-          "current_state": u"配達完了いたしました。",
-          "current_state_time": u"8/12 9:19",
-          "type": u"ペリカン便",
-          "detail": {
-            "reception_date": "8/10",
-            "delivery_time": u"8/12 午前中",
-          },
-        },
-      },
+      "result": result,
     }
 
     #u"一覧"
--- test/jpexpress_test.rb
+++ test/jpexpress_test.rb
@@ -27,4 +27,21 @@ class JpExpressJsonTest < Test::Unit::TestCase
       YAML.load_file("jpexpress/list__1.yml"),
       JSON.parse(json))
   end
+
+  def test_list__2
+    url  = @base_url + "/jpexpress/package/list.json"
+    url += "?numbers=348001739212,348001786042"
+
+    json = nil
+    assert_nothing_raised {
+      open(url) { |io|
+        assert_equal("text/javascript", io.content_type)
+        json = io.read
+      }
+    }
+
+    assert_equal(
+      YAML.load_file("jpexpress/list__2.yml"),
+      JSON.parse(json))
+  end
 end
